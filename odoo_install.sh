be6685f062a67b4bb9337f65edebb3f828b6edfe
Aarambha Bhandari <40739244+arambha21@users.noreply.github.com>
2025-02-08 21:20:33 +0545

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -128,16 +128,7 @@ if [ $IS_ENTERPRISE = "True" ]; then
     sudo su $OE_USER -c "mkdir $OE_HOME/enterprise"
     sudo su $OE_USER -c "mkdir $OE_HOME/enterprise/addons"
 
-    GITHUB_RESPONSE=$(sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/arambha21/enterprise "$OE_HOME/enterprise/addons" 2>&1)
-    while [[ $GITHUB_RESPONSE == *"Authentication"* ]]; do
-        echo "------------------------WARNING------------------------------"
-        echo "Your authentication with Github has failed! Please try again."
-        printf "In order to clone and install the Odoo enterprise version you \nneed to be an offical Odoo partner and you need access to\nhttp://github.com/odoo/enterprise.\n"
-        echo "TIP: Press ctrl+c to stop this script."
-        echo "-------------------------------------------------------------"
-        echo " "
-        GITHUB_RESPONSE=$(sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/arambha21/enterprise "$OE_HOME/enterprise/addons" 2>&1)
-    done
+    sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/arambha21/enterprise "$OE_HOME/enterprise/addons" 2>&1
 
     echo -e "\n---- Added Enterprise code under $OE_HOME/enterprise/addons ----"
     echo -e "\n---- Installing Enterprise specific libraries ----"
